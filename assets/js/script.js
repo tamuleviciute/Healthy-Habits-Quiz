acf67c556005c2120f9ec8e9cb6a0e3697cd77cd
Edita Tamuleviciute <editatamuleviciute@icloud.com>
2024-11-24 23:19:37 +0000

    Add event listeners and and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,55 @@
-function startQuiz(){
+// Wait for the page to fully load before running the quiz
+
+document.addEventListener("DOMContentLoaded", function () {
+    const rulesButton = document.getElementById("rules");
+    const scoresButton = document.getElementById("scores");
+    const startButton = document.getElementById("start");
+
+    // Add an event (click) to each button
+
+    rulesButton.addEventListener("click", function () {
+        alert("Rules for the Quiz!");
+    });
+
+    scoresButton.addEventListener("click", function () {
+        alert("Your Scores!");
+    });
+
+    startButton.addEventListener("click", function() {
+        alert("Starting the Quiz!");
+    });
+
+});
+
+
+function startQuiz() {
 
 }
 
-function showRules(){
+function showRules() {
 
 }
 
-function displayQuestion(){
+function displayQuestion() {
 
 }
 
-function checkAnswer(){
+function checkAnswer() {
 
 }
 
-function incrementScore(){
+function incrementScore() {
 
 }
 
-function endQuiz(){
+function endQuiz() {
 
 }
 
-function resetQuiz(){
+function resetQuiz() {
 
 }
 
-function toggleSound(){
+function toggleSound() {
 
 }
