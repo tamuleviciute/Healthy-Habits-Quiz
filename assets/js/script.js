bb7a5822a65a2e989a118c37444724ea12f75a5a
Edita Tamuleviciute <editatamuleviciute@icloud.com>
2024-11-24 20:02:22 +0000

    Set up structure of quiz functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function startQuiz(){
+
+}
+
+function showRules(){
+
+}
+
+function displayQuestion(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function endQuiz(){
+
+}
+
+function resetQuiz(){
+
+}
+
+function toggleSound(){
+
+}
